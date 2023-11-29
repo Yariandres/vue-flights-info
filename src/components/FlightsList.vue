7b3bc54d353508e2e3208861a0f9243586274d42
Yari Herrera <yari@primeinsights.com>
2023-11-29 22:26:29 +0100

    Refactor flight list component and update flight data

--- src/components/FlightsList.vue
+++ src/components/FlightsList.vue
@@ -12,12 +12,12 @@ defineProps<{
     <div class="col-times gap-10">
       <div class="col-left">
         <div>
-          <p class="font-size-14">
+          <p class="title text-light">
             Outgoing flight • Wednesday, {{ flight.departureDate }}
           </p>
 
           <div
-            class="flex-row gap-16"
+            class="flex-row gap-16 mt-10"
             :aria-label="`${flight.origin} - ${flight.destination}`"
           >
             <p class="text-gray font-size-12">{{ flight.origin }}</p>
@@ -28,26 +28,30 @@ defineProps<{
         <div class="flex-row gap-16">
           <div aria-label="Duration 09:45 Connection Nonstop">
             <p class="text-gray font-size-12">Departure Date</p>
-            <p class="font-size-14 text-dark">{{ flight.departureDate }}</p>
+            <p class="font-size-14 text-dark mt-10 text-center">
+              {{ flight.departureDate }}
+            </p>
           </div>
           <div aria-label="Duration 09:45">
             <p class="text-gray font-size-12">Return Date</p>
-            <p class="font-size-14 text-dark">{{ flight.returnDate }}</p>
+            <p class="font-size-14 text-dark mt-10 text-center">
+              {{ flight.returnDate }}
+            </p>
           </div>
         </div>
       </div>
-      <div class="offer align-right">
-        <div class="">
-          <p
-            class="bg-gray font-size-10 px-10 py-8 box-shadow-md border-radius-25 text-uppercase"
-          >
-            {{ flight.offerType && 'Best Price' }}
-          </p>
-        </div>
+
+      <div class="col-price offer align-right">
+        <p
+          class="bg-gray font-size-10 px-10 py-8 box-shadow-md border-radius-25 text-uppercase text-center"
+        >
+          {{ flight.offerType && 'Best Price' }}
+        </p>
+
         <div>
           <p class="text-gray font-size-12">Flight from</p>
           <p class="text-bold font-size-16 text-green">
-            <span class="font-size-10">{{ flight.price.currency }}</span>
+            <span class="font-size-12">{{ flight.price.currency && '€' }}</span>
             {{ flight.price.amount }}
           </p>
         </div>
--- src/components/pages/ShowFlights.vue
+++ src/components/pages/ShowFlights.vue
@@ -1,31 +1,159 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import type { FlightsData } from '../../types';
 import AppFilter from '../AppFilter.vue';
 import FlightsList from '../FlightsList.vue';
 
 const flights = ref<FlightsData[]>([
   {
-    origin: 'BHX',
-    destination: 'AGA',
+    origin: 'MAD',
+    destination: 'LON',
     departureDate: '2024-01-01',
     returnDate: '2024-02-01',
     price: {
-      currency: 'GBP',
+      currency: 'EUR',
       amount: 147.99,
     },
     offerType: 'BestPrice',
     uuid: '1',
   },
+  {
+    origin: 'MAD',
+    destination: 'LON',
+    departureDate: '2024-02-01',
+    returnDate: '2024-03-10',
+    price: {
+      currency: 'EUR',
+      amount: 199.99,
+    },
+    offerType: 'LastMinute',
+    uuid: '2',
+  },
+  {
+    origin: 'MAD',
+    destination: 'LON',
+    departureDate: '2024-03-01',
+    returnDate: '2024-04-10',
+    price: {
+      currency: 'EUR',
+      amount: 299.99,
+    },
+    offerType: 'BestPrice',
+    uuid: '3',
+  },
+  {
+    origin: 'MAD',
+    destination: 'LON',
+    departureDate: '2024-04-01',
+    returnDate: '2024-05-10',
+    price: {
+      currency: 'EUR',
+      amount: 249.99,
+    },
+    offerType: 'LastMinute',
+    uuid: '4',
+  },
+  {
+    origin: 'MAD',
+    destination: 'LON',
+    departureDate: '2024-05-01',
+    returnDate: '2024-06-10',
+    price: {
+      currency: 'EUR ',
+      amount: 279.99,
+    },
+    offerType: 'BestPrice',
+    uuid: '5',
+  },
+  {
+    origin: 'LON',
+    destination: 'MAD',
+    departureDate: '2024-06-01',
+    returnDate: '2024-07-10',
+    price: {
+      currency: 'EUR',
+      amount: 199.99,
+    },
+    offerType: 'BestPrice',
+    uuid: '6',
+  },
+  {
+    origin: 'LON',
+    destination: 'MAD',
+    departureDate: '2024-07-01',
+    returnDate: '2024-08-10',
+    price: {
+      currency: 'EUR',
+      amount: 249.99,
+    },
+    offerType: 'LastMinute',
+    uuid: '7',
+  },
+  {
+    origin: 'LON',
+    destination: 'MAD',
+    departureDate: '2024-08-01',
+    returnDate: '2024-09-10',
+    price: {
+      currency: 'EUR',
+      amount: 299.99,
+    },
+    offerType: 'BestPrice',
+    uuid: '8',
+  },
+  {
+    origin: 'LON',
+    destination: 'MAD',
+    departureDate: '2024-09-01',
+    returnDate: '2024-10-10',
+    price: {
+      currency: 'EUR',
+      amount: 249.99,
+    },
+    offerType: 'LastMinute',
+    uuid: '9',
+  },
+  {
+    origin: 'LON',
+    destination: 'MAD',
+    departureDate: '2024-10-01',
+    returnDate: '2024-11-10',
+    price: {
+      currency: 'EUR',
+      amount: 279.99,
+    },
+    offerType: 'BestPrice',
+    uuid: '10',
+  },
 ]);
+
+const selected = ref<string>();
+
+const filterFlights = computed(() => {
+  return flights.value.filter((flight) => {
+    if (!selected.value) return flights.value;
+
+    if (selected.value === 'origin') {
+      return flight.origin === 'LON';
+    }
+    if (selected.value === 'destination') {
+      return flight.destination === 'MAD';
+    }
+  });
+});
 </script>
 <template>
   <main class="container">
     <section class="flex-col gap-16">
       <app-filter />
+      <h1 class="text-ligth text-gray">
+        Flight results from:
+        <span class="text-dark">MAD</span> to:
+        <span class="text-dark">LON</span>
+      </h1>
       <div class="flex-col gap-10">
         <flights-list
-          v-for="flight in flights"
+          v-for="flight in filterFlights"
           :key="flight.uuid"
           :flight="flight"
         />
