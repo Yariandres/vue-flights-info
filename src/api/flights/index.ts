1fb802a8d638536df3dae379aec490d327d8c8e5
Yari Herrera <yari@primeinsights.com>
2023-11-30 01:02:08 +0100

    Add flights data and API integration

--- src/api/flights/index.ts
+++ src/api/flights/index.ts
@@ -1,1 +1,7 @@
-export const flights = 'flights';
+import { FlightsData } from '../../types';
+
+export const getFlights = async (): Promise<FlightsData[]> => {
+  const response = await fetch('http://localhost:3000/flights');
+  const data = await response.json();
+  return data;
+};
--- src/components/pages/ShowFlights.vue
+++ src/components/pages/ShowFlights.vue
@@ -1,132 +1,14 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
-import type { FlightsData } from '../../types';
 import BaseSelect from '../BaseSelect.vue';
 import FlightsList from '../FlightsList.vue';
+import { useFlightsStore } from '../../store/flights';
+import { storeToRefs } from 'pinia';
 
-const flights = ref<FlightsData[]>([
-  {
-    origin: 'MAD',
-    destination: 'LON',
-    departureDate: '2024-01-01',
-    returnDate: '2024-02-01',
-    price: {
-      currency: 'EUR',
-      amount: 147.99,
-    },
-    offerType: 'BestPrice',
-    uuid: '1',
-  },
-  {
-    origin: 'MAD',
-    destination: 'LON',
-    departureDate: '2024-02-01',
-    returnDate: '2024-03-10',
-    price: {
-      currency: 'EUR',
-      amount: 199.99,
-    },
-    offerType: 'LastMinute',
-    uuid: '2',
-  },
-  {
-    origin: 'MAD',
-    destination: 'LON',
-    departureDate: '2024-03-01',
-    returnDate: '2024-04-10',
-    price: {
-      currency: 'EUR',
-      amount: 299.99,
-    },
-    offerType: 'BestPrice',
-    uuid: '3',
-  },
-  {
-    origin: 'MAD',
-    destination: 'LON',
-    departureDate: '2024-04-01',
-    returnDate: '2024-05-10',
-    price: {
-      currency: 'EUR',
-      amount: 249.99,
-    },
-    offerType: 'LastMinute',
-    uuid: '4',
-  },
-  {
-    origin: 'MAD',
-    destination: 'LON',
-    departureDate: '2024-05-01',
-    returnDate: '2024-06-10',
-    price: {
-      currency: 'EUR ',
-      amount: 279.99,
-    },
-    offerType: 'BestPrice',
-    uuid: '5',
-  },
-  {
-    origin: 'LON',
-    destination: 'MAD',
-    departureDate: '2024-06-01',
-    returnDate: '2024-07-10',
-    price: {
-      currency: 'EUR',
-      amount: 199.99,
-    },
-    offerType: 'BestPrice',
-    uuid: '6',
-  },
-  {
-    origin: 'LON',
-    destination: 'MAD',
-    departureDate: '2024-07-01',
-    returnDate: '2024-08-10',
-    price: {
-      currency: 'EUR',
-      amount: 249.99,
-    },
-    offerType: 'LastMinute',
-    uuid: '7',
-  },
-  {
-    origin: 'LON',
-    destination: 'MAD',
-    departureDate: '2024-08-01',
-    returnDate: '2024-09-10',
-    price: {
-      currency: 'EUR',
-      amount: 299.99,
-    },
-    offerType: 'BestPrice',
-    uuid: '8',
-  },
-  {
-    origin: 'LON',
-    destination: 'MAD',
-    departureDate: '2024-09-01',
-    returnDate: '2024-10-10',
-    price: {
-      currency: 'EUR',
-      amount: 249.99,
-    },
-    offerType: 'LastMinute',
-    uuid: '9',
-  },
-  {
-    origin: 'LON',
-    destination: 'MAD',
-    departureDate: '2024-10-01',
-    returnDate: '2024-11-10',
-    price: {
-      currency: 'EUR',
-      amount: 279.99,
-    },
-    offerType: 'BestPrice',
-    uuid: '10',
-  },
-]);
+const store = useFlightsStore();
+store.getFlights();
 
+const { flights, isLoading } = storeToRefs(store);
 const selected = ref<string>('');
 
 const filterFlights = computed(() => {
@@ -143,6 +25,7 @@ const filterFlights = computed(() => {
 </script>
 <template>
   <main class="container">
+    <div v-if="isLoading">Loading flights...</div>
     <section class="flex-col gap-16">
       <base-select v-model="selected" :label="'Sort by'" />
       <h1 class="text-ligth text-gray">
